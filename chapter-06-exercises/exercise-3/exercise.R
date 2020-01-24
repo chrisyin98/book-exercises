59abdae23598f39580277422acab3bc7e5ec7936
chrisyin98 <chrisyin98@gmail.com>
2020-01-23 21:15:37 -0800

    week 3 exercises

--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -2,8 +2,8 @@
 
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
-add_three <- function(oneArgument) {
-  return(oneArgument + 3)
+add_three <- function(number) {
+  return(number + 3)
 }
 
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
