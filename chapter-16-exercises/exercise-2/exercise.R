52f4caacf1c81388ea87d8b2319bb7cb30145a9d
chrisyin98 <chrisyin98@gmail.com>
2020-02-13 12:44:02 -0800

    week 6 exercises

--- chapter-16-exercises/exercise-2/exercise.R
+++ chapter-16-exercises/exercise-2/exercise.R
@@ -3,7 +3,7 @@
 # Install and load the `ggplot2` package
 #install.packages('ggplot2')
 library("ggplot2")
-
+#Chris Yin
 # For this exercise you will again be working with the `diamonds` data set.
 # Use `?diamonds` to review details about this data set
 ?diamonds
